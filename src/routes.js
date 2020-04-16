d9f33f62f9edeb7993871a62fca4f6922093710f
Matheus Santos <dev.santos@outlook.com>
2020-04-16 20:31:31 -0300

    fix bugs routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
-const routes = require('express').Router();
-const multer = require('multer');
-const multerConfig = require('./config/multer')
 
-const Post = require('./models/Post');
+const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
 
+const Post = require("./models/Post");
 
-routes.get('/posts', async(req, res) => {
+routes.get("/posts", async (req, res) => {
     const posts = await Post.find();
-    return res.json(posts)
+
+    return res.json(posts);
 });
 
-routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
-
-    const { originalname: name, size, key, location: url = '' } = req.file;
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+    const { originalname: name, size, key, location: url = "" } = req.file;
 
     const post = await Post.create({
         name,
@@ -20,14 +20,16 @@ routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
         key,
         url
     });
+
     return res.json(post);
 });
 
-routes.delete('/posts/:id', async(req, res)=> {
-    const post = await Post.findById(req.params.id)
+routes.delete("/posts/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
     await post.remove();
 
     return res.send();
-})
+});
 
 module.exports = routes;
