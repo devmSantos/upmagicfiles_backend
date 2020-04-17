52a1d6935125ea7108b41696a1b27cf32d23bf27
Matheus Santos <dev.santos@outlook.com>
2020-04-17 18:10:30 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
+app.use(cors('https://upmagicfiles-frontend.herokuapp.com/'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
-app.use(cors());
+
 app.use(express.json());
 app.use(routes);
 app.use(express.urlencoded ({extended: true}));
