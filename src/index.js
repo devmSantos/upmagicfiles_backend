ee54677f9706e8a5c3b357f5b00195048edfc083
Matheus Santos <dev.santos@outlook.com>
2020-04-16 21:42:35 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 });
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(routes);
