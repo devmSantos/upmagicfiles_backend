b483e9e0fb13d8a3cb3b89ce5c583fba89ed2ecf
Matheus Santos <dev.santos@outlook.com>
2020-05-14 20:44:57 -0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -8,24 +8,10 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-app.use(cors('https://upmagicfiles-frontend.herokuapp.com/'));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-
 app.use(express.json());
 app.use(routes);
 app.use(express.urlencoded ({extended: true}));
 app.use(morgan('dev'));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
-
 
 
 app.listen(process.env.PORT || 3000);
